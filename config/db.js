c4217a324fc1ea8363cd1c88716c757f4b6e90b8
wathekk <wathek9711@gmail.com>
2020-10-25 15:35:48 +0100

    added test routes

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    console.log("Database connected..");
+  } catch (error) {
+    console.log(error.message);
+    //exit process
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
