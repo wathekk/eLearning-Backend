1c55b1a67019bbed6af00b0decb171064b6f44b9
wathekk <wathek9711@gmail.com>
2020-10-25 20:14:27 +0100

    test

--- routes/api/users.js
+++ routes/api/users.js
@@ -80,7 +80,7 @@ router.post(
       );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send("server error");
+      res.status(500).send("server errorr");
     }
   }
 );
